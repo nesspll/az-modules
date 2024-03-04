1a20cac7a0fe8eb7e2a156f0badcf59638e5a4c2
nesspll <nesim@cuburn.com>
2024-03-04 10:57:35 +0100

    sig change

--- mods/virtual_machine/outputs.tf
+++ mods/virtual_machine/outputs.tf
@@ -2,19 +2,3 @@
 output "vm_name" {
   value = azurerm_linux_virtual_machine.vm-machine.name
 }
-
-
-output "resource_group_name" {
-  value = azurerm_resource_group.rg-vm-testing.name
-  description = "The name of the resource group"
-}
-
-output "resource_group_location" {
-  value = azurerm_resource_group.rg-vm-testing.location
-  description = "The location of the resource group"
-}
-
-output "resource_group_id" {
-  value = azurerm_resource_group.rg-vm-testing.id
-  description = "The ID of the resource group"
-}
